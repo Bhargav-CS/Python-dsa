2232a11a3450a035dfe8ae3ac918b7c7df041bca
Bhargav-CS <bhargavpatki@gmail.com>
2024-03-10 23:56:10 +0530

    gfg remove duplicates

--- Demo.py
+++ Demo.py
@@ -1,9 +1,18 @@
-s = "geEksforGEeks"
+class Solution:
+    def duplicates(self, arr, n):
+        seen = set()
+        duplicates = set()
+        for num in arr:
+            if num in seen:
+                duplicates.add(num)
+            else:
+                seen.add(num)
+        return sorted(list(duplicates)) or [-1]
 
-p = ""
 
-for i in s:
-    if i not in p:
-        p += i
-
-print(p)    
+# arr = [0,3,1,2]
+# n = 4
+arr = [2, 3, 1, 2, 3]
+n = 5
+sol = Solution()
+print(sol.duplicates(arr, n)) # 2 3
