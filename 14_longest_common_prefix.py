39d2cf68eb7c77a305efa1838280155c99654fb9
Bhargav-CS <bhargavpatki@gmail.com>
2024-04-12 23:33:15 +0530

    LC 14

---
+++ 14_longest_common_prefix.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        prefix = ""
+        if len(strs) == 0:
+            return prefix
+        for i in range(len(strs[0])):
+            c = strs[0][i]
+            for j in range(1, len(strs)):
+                if i >= len(strs[j]) or strs[j][i] != c:
+                    return prefix
+            prefix += c
+        return prefix
+        
+
+strs = ["flower","flow","flight"]
+sol = Solution()
+print(sol.longestCommonPrefix(strs))    # Output: "fl"
