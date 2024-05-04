22ec610fb882221389e1c654529321fd7c830308
Bhargav-CS <bhargavpatki@gmail.com>
2024-05-04 21:48:40 +0530

    gfg reverse llist

---
+++ reverse_llist.py
@@ -0,0 +1,27 @@
+"""
+# Node Class
+
+class node:
+    def __init__(self, val):
+        self.data = val
+        self.next = None
+
+"""
+
+class Solution:
+    #Function to reverse a linked list.
+    def reverseList(self, head):
+        # Code here
+        prev = None
+        current = head
+        while current is not None:
+            next = current.next
+            current.next = prev
+            prev = current
+            current = next
+        head = prev
+        return head
+    
+    
+    
+    
