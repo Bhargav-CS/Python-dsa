5deb6f618afbc58a26b349645f46a4c30984998a
Bhargav-CS <bhargavpatki@gmail.com>
2024-05-11 23:56:08 +0530

    LC 202

---
+++ 202_happy_number.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        def get_next(n):
+            total_sum = 0
+            while n > 0:
+                n, digit = divmod(n, 10)
+                total_sum += digit ** 2
+            return total_sum
+        
+        seen = set()
+        while n != 1 and n not in seen:
+            seen.add(n)
+            n = get_next(n)
+        return n == 1
+    
+    
+    
+n = 19
+sol = Solution()
+print(sol.isHappy(n)) #True
