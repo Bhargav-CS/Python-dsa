d0f0272beb1607ddd8653a5272a13e57ba98a252
Bhargav-CS <bhargavpatki@gmail.com>
2024-05-17 22:35:44 +0530

    gfg find pair given difference

---
+++ find_pair_given_difference.py
@@ -0,0 +1,26 @@
+from typing import List
+
+class Solution:
+    def findPair(self, n : int, x : int, arr : List[int]) -> int:
+        # code here
+        arr.sort()
+        i = 0
+        j = 1
+        while i < n and j < n:
+            if i != j and arr[j] - arr[i] == x:
+                return 1
+            elif arr[j] - arr[i] < x:
+                j += 1
+            else:
+                i += 1
+        return -1
+    
+    
+    
+    
+n = 6
+x = 78
+arr = [5, 20, 3, 2, 5, 80]
+
+sol = Solution()
+print(sol.findPair(n, x, arr)) # 1
