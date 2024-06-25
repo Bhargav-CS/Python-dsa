48a6b778bf9595edd0b7daf696919011735cbec4
Bhargav-CS <bhargavpatki@gmail.com>
2024-06-25 12:02:57 +0530

    revising the problem

--- 42_PT_trapping_rain_water.py
+++ 42_PT_trapping_rain_water.py
@@ -1,8 +1,8 @@
 class Solution(object):
     def trap(self, height):
-        if not height : return 0
+        if not height:return 0 
         
-        l,r = 0, len(height)-1
+        l, r = 0, len(height)-1
         leftMax, rightMax = height[l], height[r]
         res = 0
         
@@ -11,14 +11,12 @@ class Solution(object):
                 l +=1
                 leftMax = max(leftMax, height[l])
                 res += leftMax - height[l]
-            
             else:
                 r-= 1
                 rightMax = max(rightMax, height[r])
                 res += rightMax - height[r]
-        
-        return res
                 
+        return res
     
 height = [0,1,0,2,1,0,1,3,2,1,2,1]
 sol = Solution()
