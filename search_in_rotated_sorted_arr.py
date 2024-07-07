453fe318ed9c3283e0247af901fb1f88bc902595
Bhargav-CS <bhargavpatki@gmail.com>
2024-07-08 01:15:58 +0530

    gfg search in rotated sorted arr

---
+++ search_in_rotated_sorted_arr.py
@@ -0,0 +1,15 @@
+class Solution:
+    def search(self, arr, key):
+        try:
+            return arr.index(key)
+        except ValueError:
+            return -1
+            
+    
+    
+# arr = [5, 6, 7, 8, 9, 10, 1, 2, 3]
+# key = 10
+arr = [3, 5, 1, 2]
+key = 6
+sol = Solution()
+print(sol.search(arr, key))
