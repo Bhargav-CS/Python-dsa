51b2df44e15981a15ebd8272d95b389eed29b019
Bhargav-CS <bhargavpatki@gmail.com>
2024-07-08 20:19:55 +0530

    LC practice 88 merge sorted arrays

--- Demo.py
+++ Demo.py
@@ -1,2 +1,17 @@
-bhargav = "bhargav"
-print(bhargav.startswith("bhar"))
+class Solution:
+    def merge(self, nums1, m, nums2, n):
+        pass
+    
+    
+nums1 = [0]
+m = 0
+nums2 = [1]
+n = 1
+sol = Solution()
+print(sol.merge(nums1, m, nums2, n))
+
+for i in range(n):
+    nums1.remove(0)
+    
+nums1.extend(nums2)
+print(nums1)
