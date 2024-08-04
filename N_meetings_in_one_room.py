90fc9a1be1f202cc3be1cdab9364e5ad041a7cb3
Bhargav-CS <bhargavpatki@gmail.com>
2024-08-04 18:49:44 +0530

    gfg n meeting in one room

--- N_meetings_in_one_room.py
+++ N_meetings_in_one_room.py
@@ -1,23 +1,17 @@
 class Solution:
     def maximumMeetings(self,n,start,end):
         # Code here
-        # Create a list of tuples with the index, start time, and end time
         meetings = [(i, start[i], end[i]) for i in range(n)]
-        # Sort the meetings by their end time
         meetings.sort(key=lambda x: x[2])
-        # Initialize the count of meetings
         count = 1
-        # Initialize the end time of the first meeting
         end_time = meetings[0][2]
-        # Iterate through the meetings
         for i in range(1, n):
-            # If the start time of the current meeting is greater than or equal to the end time of the previous meeting
             if meetings[i][1] > end_time:
-                # Increment the count of meetings
+                
                 count += 1
-                # Update the end time of the previous meeting
+                
                 end_time = meetings[i][2]
-        # Return the count of meetings
+        
         return count
             
     
