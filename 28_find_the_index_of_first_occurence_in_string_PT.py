d2b95fa20e23be50a8ed5a65acdabc50f3a44474
Bhargav-CS <bhargavpatki@gmail.com>
2024-08-06 22:18:23 +0530

    LC 28

---
+++ 28_find_the_index_of_first_occurence_in_string_PT.py
@@ -0,0 +1,9 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        return heystack.find(needle)
+    
+
+heystack = "sadbutsad"
+needle = "sad"
+sol = Solution()
+print(sol.strStr(heystack, needle))
