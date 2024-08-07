d22962b87120c14c4200f90112b43cafa62d4460
Bhargav-CS <bhargavpatki@gmail.com>
2024-08-07 13:01:53 +0530

    gfg kth element of two arrays

---
+++ kth_element_of_two_arrays.py
@@ -0,0 +1,12 @@
+class Solution:
+    def kthElement(self, k, arr1, arr2):
+        arr1.extend(arr2)
+        arr1.sort()
+        print(arr1)
+        return arr1[k-1]
+    
+k = 5
+arr1 = [2, 3, 6, 7, 9]
+arr2 = [1, 4, 8, 10]
+sol = Solution()
+print(sol.kthElement(k, arr1, arr2))
