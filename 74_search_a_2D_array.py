2726dd419756fbe197ba0847c22c23b138dcd5ca
Bhargav-CS <bhargavpatki@gmail.com>
2024-08-10 10:40:24 +0530

    LC 74

--- 74_search_a_2D_array.py
+++ 74_search_a_2D_array.py
@@ -18,10 +18,11 @@ class Solution:
                 right = mid - 1
         return False
     
-        
-            
+   
                     
 matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
 target = 3
 sol = Solution()
 print(sol.searchMatrix(matrix, target)) # True
+
+#trying this question once again
