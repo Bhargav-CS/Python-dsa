5653d7cbaa4b5ec9c61b6afc3c8eaca329a4f322
Bhargav-CS <bhargavpatki@gmail.com>
2024-09-27 21:42:40 +0530

    gfg k sized subarray

---
+++ k_sized_subarray_maximum.py
@@ -0,0 +1,20 @@
+class Solution:
+    
+    #Function to find maximum of each subarray of size k.
+    def max_of_subarrays(self,k,arr):
+        
+        n = len(arr)
+        list2 = []
+
+        current_max = max(arr[:k])
+        list2.append(current_max)
+        
+        for i in range(1, n - k + 1):
+            if arr[i - 1] == current_max:
+                current_max = max(arr[i:i + k])
+            else:
+                current_max = max(current_max, arr[i + k - 1])
+            
+            list2.append(current_max)
+        
+        return list2
