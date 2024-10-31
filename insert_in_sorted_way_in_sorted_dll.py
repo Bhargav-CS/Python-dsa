a3fd656d08980d9c7dde6b570c972981a5ba63ea
Bhargav-CS <bhargavpatki@gmail.com>
2024-10-31 17:50:05 +0530

    gfg insert in sorted potd

---
+++ insert_in_sorted_way_in_sorted_dll.py
@@ -0,0 +1,27 @@
+'''
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.prev = None
+        self.next = None
+'''
+class Solution:
+    #Function to insert a node in a sorted doubly linked list.
+    def sortedInsert(self, head, x):    
+        new_node = Node(x)
+        if head is None or head.data >= x:
+            new_node.next = head
+            if head:
+                head.prev = new_node
+            return new_node          
+        current = head
+        while current.next and current.next.data < x:
+            current = current.next
+
+        new_node.next = current.next
+        if current.next:
+            current.next.prev = new_node
+        new_node.prev = current
+        current.next = new_node
+
+        return head 
