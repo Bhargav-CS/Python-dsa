97d03c307d28fa4d44951853fbf868385557867f
Bhargav-CS <bhargavpatki@gmail.com>
2024-12-16 21:41:36 +0530

    kth element of two arrays

--- kth_element_of_two_arrays.py
+++ kth_element_of_two_arrays.py
@@ -5,6 +5,16 @@ class Solution:
         print(arr1)
         return arr1[k-1]
     
+
+class Solution:
+
+    def kthElement(self, a, b, k):
+        
+        a.extend(b)
+        a.sort()
+        return a[k-1]
+
+
 k = 5
 arr1 = [2, 3, 6, 7, 9]
 arr2 = [1, 4, 8, 10]
