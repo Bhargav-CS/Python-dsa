7b20c9e257993bb173ffb27e837c2a20915b58d7
Bhargav-CS <bhargavpatki@gmail.com>
2024-12-24 20:21:47 +0530

    search in sorted mat

---
+++ search_in_sorted_mat.py
@@ -0,0 +1,13 @@
+def searchMatrix(self, mat, x):
+    rows,cols=len(mat),len(mat[0])
+    l,h=0,rows*cols-1
+    while l<=h:
+        mid=(l+h)//2
+        i,j=mid//cols,mid%cols
+        if mat[i][j]==x:
+            return True
+        elif mat[i][j]>x:
+            h=mid-1
+        else:
+            l=mid+1
+    return False
