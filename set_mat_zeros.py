f6ef37df7266fd3f5928fd54e13315165a610eec
Bhargav-CS <bhargavpatki@gmail.com>
2024-12-25 22:39:30 +0530

    set mat zeros

---
+++ set_mat_zeros.py
@@ -0,0 +1,19 @@
+def setMatrixZeroes(mat):
+    rows = len(mat)
+    cols = len(mat[0])
+    
+    zero_rows = set()
+    zero_cols = set()
+
+    for i in range(rows):
+        for j in range(cols):
+            if mat[i][j] == 0:
+                zero_rows.add(i)
+                zero_cols.add(j)
+
+    for i in range(rows):
+        for j in range(cols):
+            if i in zero_rows or j in zero_cols:
+                mat[i][j] = 0
+
+    return mat
