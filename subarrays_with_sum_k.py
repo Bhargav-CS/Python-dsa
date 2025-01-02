e3861358b64df6da5d9b78e006690ef3b49672e5
Bhargav-CS <bhargavpatki@gmail.com>
2025-01-02 19:21:20 +0530

    subarrays with sum k

---
+++ subarrays_with_sum_k.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countSubarrays(self, arr, k):
+        d={0:1}
+        s=0
+        count=0
+        for i in arr:
+            s+=i
+            count+=d.get(s-k,0)
+            d[s]=d.get(s,0)+1
+        return count 
